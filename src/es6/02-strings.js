65464c6731cbadc482360291c659f6fff299411e
Ivan Adamo <ivan.adamo.8@gmail.com>
2023-01-21 21:33:00 -0300

    strings

---
+++ src/es6/02-strings.js
@@ -0,0 +1,18 @@
+let hello = 'Hello';
+let world = 'World';
+let epicPhrase = hello + ' ' + world + '!';
+console.log(epicPhrase);
+
+// Template literals
+
+let epicPhrase2 = `${hello} ${world}!`;
+console.log(epicPhrase2);
+
+// Multi-line strings
+
+let lorem = 'esto es una linea \n' + 'esto es otra linea';
+console.log(lorem);
+
+let lorem2 = `Esto es una frase de mmierda
+y esta es la segunda linea de la frase de mierda`;
+console.log(lorem2);
