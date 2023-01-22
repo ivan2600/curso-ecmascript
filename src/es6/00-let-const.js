772744167a1dc4e93ea99b5f1af4b040b7e64f77
Ivan Adamo <ivan.adamo.8@gmail.com>
2023-01-21 21:19:33 -0300

    initial files + arrow functions

---
+++ src/es6/00-let-const.js
@@ -0,0 +1,13 @@
+const fruits = () => {
+  if(true) {
+    var fruit1 = 'Apple'; // function scope
+    let fruit2 = 'Kiwi'; // block scope 
+    const fruit3 = 'Banana'; // block scope
+  }
+  console.log(fruit1);
+  console.log(fruit2);
+  console.log(fruit3);
+
+}
+
+fruits();
