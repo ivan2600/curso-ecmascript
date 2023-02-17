f4deb85cc9a0b23ee297a9811aaa2e3348c364a6
Ivan Adamo <ivan.adamo.8@gmail.com>
2023-02-17 18:06:01 -0300

    clases

--- src/es6/08-clases.js
+++ src/es6/08-clases.js
@@ -47,3 +47,28 @@ console.log(ana.greeting);
 
 // setters getters
 
+class User {
+  // constructor
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+  // metodos
+  speak() {
+    return 'Hello';
+  }
+  greeting() {
+    return `${this.speak()} ${this.name}`;
+  }
+  get uAge() {
+    return this.age;
+  }
+  set uAge(n) {
+    this.age = n;
+  }
+}
+
+const lololoper = new User('Larry', 23);
+console.log(lololoper.uAge);
+console.log(lololoper.uAge = 20);
+
