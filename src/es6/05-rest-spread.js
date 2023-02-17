574baaa244bc5dae2c3e5f5f48ce8b01c35c50a2
Ivan Adamo <ivan.adamo.8@gmail.com>
2023-02-17 16:34:19 -0300

    rest spread 2

--- src/es6/05-rest-spread.js
+++ src/es6/05-rest-spread.js
@@ -1,7 +1,34 @@
-// arrays destructure
+// arrays destructuring
 
 let killEmAll = ['Hit the lights', 'Seek and Destroy'];
 
 let [a, b] = killEmAll;
 
 console.log(a, b);
+
+// objects destructuring
+
+let album = { title: 'Kill Em All', age: 1983 };
+
+let { title, age } = album;
+
+console.log(title, age);
+
+// spread operator
+
+let gender = { nombre: 'Thrash Metal', example: 'Megadeth'};
+let topSong = 'Holy Wars';
+
+let data = { id: 1, ...gender, topSong };
+
+console.log(data);
+
+// rest
+
+function sum(num, ...values) {
+  console.log(values);
+  console.log(num + values[0]);
+  return num + values[0];
+}
+
+sum(1, 1, 2, 3);
