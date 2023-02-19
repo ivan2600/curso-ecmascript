aa188cd0c826e5a064645010abd8f9bc3b281e7c
Ivan Adamo <ivan.adamo.8@gmail.com>
2023-02-19 01:19:52 -0300

    trailing commas

---
+++ src/es8/02-string-padding.js
@@ -0,0 +1,4 @@
+const string = "Hey Bulldog";
+
+console.log(string.padStart(40, ', tremendo temón!'));
+console.log(string.padEnd(40, ', tremendo temón!'));
---
+++ src/es8/04-trailing-commas.js
@@ -0,0 +1,4 @@
+const age = [26, 31, 40, 35, , , , 36];
+
+console.log(age);
+console.log(age.length);
